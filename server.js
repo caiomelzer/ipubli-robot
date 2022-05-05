23640b4c5d4cda3d839613f8570b3c3b8534ef19
Caio Melzer <melzer.caio@gmail.com>
2022-05-05 11:01:28 -0300

    MVP works!

--- server.js
+++ server.js
@@ -8,33 +8,48 @@ const axios = require("axios");
 var fs = require('fs');
 const { server } = require('./config');
 
-cron.schedule("* * * * *", () => {
+cron.schedule("* * * * * *", () => {
     const optionsUsername = {
         method: 'GET',
         url: server.url+'/api/v1/utils/instagram/',
         headers: {}
       };
+      
       axios.request(optionsUsername).then(function (responseUsername) {
-        console.log('chamendo', responseUsername.data.username)
-        let options = {
-            method: 'GET',
-            url: server.url+'/api/v1/utils/instagram/'+responseUsername.data.username,
-            headers: {}
-        };
-        axios.request(options).then(function (response) {
-            let optionsUpdateUser = {
-                method: 'PUT',
-                url: server.url+'/api/v1/utils/instagram/'+responseUsername.data.username,
-                headers: {}
-            };
-            axios.request(optionsUpdateUser).then(function (responseUsername) {
-                console.log('chamendo', responseUsername.data.username)
-            }).catch(function (error) {
-                console.log(error)
-            });
-        }).catch(function (error) {
-            console.log(error)
-        });
+        console.log('Calling Instragram Profile', responseUsername.data.updatedAt)
+         var updatedAt = new Date(responseUsername.data.updatedAt);   
+            var today = new Date(Date.now());
+            today = today.setDate(today.getDate() - 30);
+            console.log(updatedAt , today)
+            if(updatedAt < today){
+                console.log('chamar')
+                let options = {
+                    method: 'GET',
+                    url: server.url+'/api/v1/utils/instagram/'+responseUsername.data.username,
+                    headers: {}
+                };
+                axios.request(options).then(function (response) {
+                    let optionsUpdateUser = {
+                        method: 'PUT',
+                        url: server.url+'/api/v1/utils/instagram/'+responseUsername.data.username,
+                        headers: {}
+                    };
+                    axios.request(optionsUpdateUser).then(function (responseUsername) {
+                        console.log('chamendo', responseUsername.data.username)
+                    }).catch(function (error) {
+                        console.log(error)
+                    });
+                }).catch(function (error) {
+                    console.log(error)
+                });
+            }
+            else{
+                console.log('No updates needed')
+            }
+
+            
+            
+        
     }).catch(function (error) {
         console.log(error)
     });
