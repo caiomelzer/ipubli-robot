b1e0f7d4738257da90249bdb503eaef00ffb6f3f
Caio Melzer <melzer.caio@gmail.com>
2022-05-05 09:58:54 -0300

    Change to env

--- server.js
+++ server.js
@@ -6,24 +6,25 @@ const bodyParser = require('body-parser');
 const cron = require("node-cron");
 const axios = require("axios");
 var fs = require('fs');
+const { server } = require('./config');
 
-cron.schedule("5 * * * * *", () => {
+cron.schedule("* * * * *", () => {
     const optionsUsername = {
         method: 'GET',
-        url: 'http://localhost:4000/api/v1/utils/instagram/',
+        url: server.url+'/api/v1/utils/instagram/',
         headers: {}
       };
       axios.request(optionsUsername).then(function (responseUsername) {
         console.log('chamendo', responseUsername.data.username)
         let options = {
             method: 'GET',
-            url: 'http://localhost:4000/api/v1/utils/instagram/'+responseUsername.data.username,
+            url: server.url+'/api/v1/utils/instagram/'+responseUsername.data.username,
             headers: {}
         };
         axios.request(options).then(function (response) {
             let optionsUpdateUser = {
                 method: 'PUT',
-                url: 'http://localhost:4000/api/v1/utils/instagram/'+responseUsername.data.username,
+                url: server.url+'/api/v1/utils/instagram/'+responseUsername.data.username,
                 headers: {}
             };
             axios.request(optionsUpdateUser).then(function (responseUsername) {
